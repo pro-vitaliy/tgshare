c3fe56249f3d1f518c43f21776e4d31cbd8c1bfb
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-05-19 23:11:38 +0500

    feat: add controller for processing incoming updates

---
+++ dispatcher/src/main/java/com/github/provitaliy/controller/UpdateController.java
@@ -0,0 +1,82 @@
+package com.github.provitaliy.controller;
+
+import com.github.provitaliy.configuration.QueueProperties;
+import com.github.provitaliy.service.UpdateProducer;
+import com.github.provitaliy.utils.MessageUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class UpdateController {
+    private final MessageUtils messageUtils;
+    private final TelegramClient telegramClient;
+    private final UpdateProducer updateProducer;
+    private final QueueProperties queueProperties;
+
+    public void processUpdate(Update update) {
+        if (update == null) {
+            log.error("Received update is null");
+            return;
+        }
+
+        if (update.getMessage() != null) {
+            distributeMessagesByType(update);
+        } else {
+            log.error("Received unsupported message type {}", update);
+        }
+    }
+
+    private void distributeMessagesByType(Update update) {
+        var message = update.getMessage();
+        if (message.hasText()) {
+            processTextMessage(update);
+        } else if (message.hasDocument()) {
+            processDocMessage(update);
+        } else if (message.hasPhoto()) {
+            processPhotoMessage(update);
+        } else {
+            setUnsupportedTypeView(update);
+        }
+    }
+
+    private void setUnsupportedTypeView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update, "Unsupported message type");
+        setView(sendMessage);
+    }
+
+    private void setView(SendMessage sendMessage) {
+        try {
+            telegramClient.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("Failed to send message", e);
+        }
+    }
+
+    private void setFileIsReceivedView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update,
+                "File received. Processing in progress");
+        setView(sendMessage);
+    }
+
+    private void processPhotoMessage(Update update) {
+        updateProducer.produce(queueProperties.getPhotoMessageUpdate(), update);
+        setFileIsReceivedView(update);
+    }
+
+    private void processDocMessage(Update update) {
+        updateProducer.produce(queueProperties.getDocMessageUpdate(), update);
+        setFileIsReceivedView(update);
+    }
+
+    private void processTextMessage(Update update) {
+        updateProducer.produce(queueProperties.getTextMessageUpdate(), update);
+        setFileIsReceivedView(update);
+    }
+}
