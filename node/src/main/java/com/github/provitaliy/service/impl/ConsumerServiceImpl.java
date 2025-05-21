70b6fe15a4958df1bb5c9e5e063b174d9e6d4cce
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-05-21 23:22:25 +0500

    feat: add ConsumerService interface in Node with implementation

---
+++ node/src/main/java/com/github/provitaliy/service/impl/ConsumerServiceImpl.java
@@ -0,0 +1,42 @@
+package com.github.provitaliy.service.impl;
+
+import com.github.provitaliy.service.ConsumerService;
+import com.github.provitaliy.service.ProducerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ConsumerServiceImpl implements ConsumerService {
+    private final ProducerService producerService;
+
+    @Override
+    @RabbitListener(queues = "${spring.rabbitmq.queues.text-message-update}")
+    public void consumeTextMessageUpdates(Update update) {
+        log.debug("NODE: Text message is received");
+
+        SendMessage sendMessage = SendMessage.builder()
+                .chatId(update.getMessage().getChatId())
+                .text("Hello from NODE")
+                .build();
+
+        producerService.produceAnswer(sendMessage);
+    }
+
+    @Override
+    @RabbitListener(queues = "${spring.rabbitmq.queues.doc-message-update}")
+    public void consumeDocMessageUpdates(Update update) {
+        log.debug("NODE: Doc message is received");
+    }
+
+    @Override
+    @RabbitListener(queues = "${spring.rabbitmq.queues.photo-message-update}")
+    public void consumePhotoMessageUpdates(Update update) {
+        log.debug("NODE: Photo message is received");
+    }
+}
