69b788498a66ae576b095d2cda8293ca04b66cb7
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-05-23 23:57:33 +0500

    feat: add RawData entity

---
+++ node/src/main/java/com/github/provitaliy/entity/RawData.java
@@ -0,0 +1,34 @@
+package com.github.provitaliy.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "raw_data")
+public class RawData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private Update event;
+}
