e62117f0ee7cc44f832aa8b6ceb9fc644ee4d0ab
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-05-24 18:09:04 +0500

    feat:  add MainService to ConsumerService and update consumeTextMessageUpdates method

--- node/src/main/java/com/github/provitaliy/service/impl/ConsumerServiceImpl.java
+++ node/src/main/java/com/github/provitaliy/service/impl/ConsumerServiceImpl.java
@@ -1,31 +1,25 @@
 package com.github.provitaliy.service.impl;
 
 import com.github.provitaliy.service.ConsumerService;
-import com.github.provitaliy.service.ProducerService;
+import com.github.provitaliy.service.MainService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ConsumerServiceImpl implements ConsumerService {
-    private final ProducerService producerService;
+    private final MainService mainService;
 
     @Override
     @RabbitListener(queues = "${spring.rabbitmq.queues.text-message-update}")
     public void consumeTextMessageUpdates(Update update) {
         log.debug("NODE: Text message is received");
 
-        SendMessage sendMessage = SendMessage.builder()
-                .chatId(update.getMessage().getChatId())
-                .text("Hello from NODE")
-                .build();
-
-        producerService.produceAnswer(sendMessage);
+        mainService.processTextMessage(update);
     }
 
     @Override
