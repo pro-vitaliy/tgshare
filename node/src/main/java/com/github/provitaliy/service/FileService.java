6979eb7ca6275e4ff3ba8430ef083b9026698f7b
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-06-07 21:17:35 +0500

    feat(node): add photo process method to FileService

--- node/src/main/java/com/github/provitaliy/service/FileService.java
+++ node/src/main/java/com/github/provitaliy/service/FileService.java
@@ -1,8 +1,10 @@
 package com.github.provitaliy.service;
 
 import com.github.provitaliy.entity.AppDocument;
+import com.github.provitaliy.entity.AppPhoto;
 import org.telegram.telegrambots.meta.api.objects.message.Message;
 
 public interface FileService {
-    AppDocument processDoc(Message externalMessage);
+    AppDocument processDoc(Message telegramMessage);
+    AppPhoto processPhoto(Message telegramMessage);
 }
--- node/src/main/java/com/github/provitaliy/service/impl/FileServiceImpl.java
+++ node/src/main/java/com/github/provitaliy/service/impl/FileServiceImpl.java
@@ -1,29 +1,33 @@
 package com.github.provitaliy.service.impl;
 
 import com.github.provitaliy.dao.AppDocumentDAO;
+import com.github.provitaliy.dao.AppPhotoDAO;
 import com.github.provitaliy.dao.BinaryContentDAO;
 import com.github.provitaliy.entity.AppDocument;
+import com.github.provitaliy.entity.AppPhoto;
 import com.github.provitaliy.entity.BinaryContent;
 import com.github.provitaliy.service.FileService;
 import com.github.provitaliy.service.TelegramService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.message.Message;
 
 @Service
 @RequiredArgsConstructor
 public class FileServiceImpl implements FileService {
     private final AppDocumentDAO appDocumentDAO;
+    private final AppPhotoDAO appPhotoDAO;
     private final BinaryContentDAO binaryContentDAO;
     private final TelegramService telegramService;
 
     @Override
-    public AppDocument processDoc(Message externalMessage) {
-        var tgDoc = externalMessage.getDocument();
+    public AppDocument processDoc(Message telegramMessage) {
+        var tgDoc = telegramMessage.getDocument();
         byte[] fileInBytes = telegramService.downloadFileById(tgDoc.getFileId());
         var binaryContent = binaryContentDAO.save(new BinaryContent(fileInBytes));
         var appDoc = AppDocument.builder()
-                .telegramField(tgDoc.getFileId())
+                .telegramFileId(tgDoc.getFileId())
                 .docName(tgDoc.getFileName())
                 .binaryContent(binaryContent)
                 .mimeType(tgDoc.getMimeType())
@@ -31,5 +35,19 @@ public class FileServiceImpl implements FileService {
                 .build();
 
         return appDocumentDAO.save(appDoc);
+    }
+
+    @Override
+    public AppPhoto processPhoto(Message telegramMessage) {
+        //TODO пока обрабоатываем только одно фото
+        PhotoSize tgPhoto = telegramMessage.getPhoto().getFirst();
+        byte[] fileInBytes = telegramService.downloadFileById(tgPhoto.getFileId());
+        var binaryContent = binaryContentDAO.save(new BinaryContent(fileInBytes));
+        var appPhoto = AppPhoto.builder()
+                .telegramFileId(tgPhoto.getFileId())
+                .binaryContent(binaryContent)
+                .fileSize(tgPhoto.getFileSize())
+                .build();
+        return appPhotoDAO.save(appPhoto);
     }
 }
