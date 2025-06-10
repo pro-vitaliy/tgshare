6453ba628fc7b8864910f4b8f9eb2e576e7c3414
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-06-10 23:10:32 +0500

    feat(rest-service): add custom not found exception & global handler

---
+++ rest-service/src/main/java/com/github/provitaliy/handler/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.github.provitaliy.handler;
+
+import com.github.provitaliy.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+}
