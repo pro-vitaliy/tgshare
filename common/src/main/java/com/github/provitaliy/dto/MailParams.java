5211aa357d8d9e4866881635e9de40384f4f8c1f
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-06-12 22:42:08 +0500

    feat(common): add MailParams dto

---
+++ common/src/main/java/com/github/provitaliy/dto/MailParams.java
@@ -0,0 +1,15 @@
+package com.github.provitaliy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MailParams {
+    private String id;
+    private String mailTo;
+}
