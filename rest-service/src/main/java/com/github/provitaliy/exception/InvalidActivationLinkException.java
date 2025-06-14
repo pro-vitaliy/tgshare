cdecfe7af59d7bc3c9f5d3d95a93586c40975df9
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-06-14 15:25:26 +0500

    feat(rest-service): add InvalidActivationLinkException

---
+++ rest-service/src/main/java/com/github/provitaliy/exception/InvalidActivationLinkException.java
@@ -0,0 +1,7 @@
+package com.github.provitaliy.exception;
+
+public class InvalidActivationLinkException extends RuntimeException {
+    public InvalidActivationLinkException(String message) {
+        super(message);
+    }
+}
--- rest-service/src/main/java/com/github/provitaliy/handler/GlobalExceptionHandler.java
+++ rest-service/src/main/java/com/github/provitaliy/handler/GlobalExceptionHandler.java
@@ -1,17 +1,27 @@
 package com.github.provitaliy.handler;
 
+import com.github.provitaliy.exception.InvalidActivationLinkException;
 import com.github.provitaliy.exception.ResourceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Slf4j
+    @ControllerAdvice
+    public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        @ExceptionHandler(ResourceNotFoundException.class)
+        public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
+            log.warn("Пользователь не найден, {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Такого пользователя не существует.");
+        }
+
+        @ExceptionHandler(InvalidActivationLinkException.class)
+        public ResponseEntity<String> handleInvalidActivationLinkException(InvalidActivationLinkException e) {
+            log.warn("Ошибка активации, {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Ссылка активации недействительна.");
+        }
     }
-}
