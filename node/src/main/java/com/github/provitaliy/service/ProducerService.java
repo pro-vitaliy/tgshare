ae10964b16207abc58fa5665ac6b17ee03fa4ee8
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-06-22 02:14:06 +0500

    feat(node): add produce email registration method in ProducerService

--- node/src/main/java/com/github/provitaliy/service/ProducerService.java
+++ node/src/main/java/com/github/provitaliy/service/ProducerService.java
@@ -1,7 +1,9 @@
 package com.github.provitaliy.service;
 
+import com.github.provitaliy.dto.MailParams;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 public interface ProducerService {
     void produceAnswer(SendMessage sendMessage);
+    void produceRegistrationMail(MailParams mailParams);
 }
--- node/src/main/java/com/github/provitaliy/service/impl/ProducerServiceImpl.java
+++ node/src/main/java/com/github/provitaliy/service/impl/ProducerServiceImpl.java
@@ -1,5 +1,6 @@
 package com.github.provitaliy.service.impl;
 
+import com.github.provitaliy.dto.MailParams;
 import com.github.provitaliy.service.ProducerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -14,10 +15,18 @@ public class ProducerServiceImpl implements ProducerService {
     @Value("${spring.rabbitmq.queues.answer-message}")
     private String answerQueue;
 
+    @Value("${spring.rabbitmq.queues.registration-mail}")
+    private String emailRegisterQueue;
+
     private final RabbitTemplate rabbitTemplate;
 
     @Override
     public void produceAnswer(SendMessage sendMessage) {
         rabbitTemplate.convertAndSend(answerQueue, sendMessage);
+    }
+
+    @Override
+    public void produceRegistrationMail(MailParams mailParams) {
+        rabbitTemplate.convertAndSend(emailRegisterQueue, mailParams);
     }
 }
