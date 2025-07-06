4afb9f07cb8900d9a7a64c3c0cf6b4906b6e1d2d
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-07-07 00:59:29 +0500

    refactor(rest-service): remove InvalidActivationLinkException

--- rest-service/src/main/java/com/github/provitaliy/exception/InvalidActivationLinkException.java
+++
@@ -1,7 +0,0 @@
-package com.github.provitaliy.exception;
-
-public class InvalidActivationLinkException extends RuntimeException {
-    public InvalidActivationLinkException(String message) {
-        super(message);
-    }
-}
