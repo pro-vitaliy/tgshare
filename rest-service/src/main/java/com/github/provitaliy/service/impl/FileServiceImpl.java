6dc781b43ea84bbe01991bbff37651b65e02542c
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-07-07 01:00:29 +0500

    feat(rest-service): add id decoding in FileService

--- rest-service/src/main/java/com/github/provitaliy/service/impl/FileServiceImpl.java
+++ rest-service/src/main/java/com/github/provitaliy/service/impl/FileServiceImpl.java
@@ -6,6 +6,7 @@ import com.github.provitaliy.entity.AppDocument;
 import com.github.provitaliy.entity.AppPhoto;
 import com.github.provitaliy.exception.ResourceNotFoundException;
 import com.github.provitaliy.service.FileService;
+import com.github.provitaliy.utils.Decoder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -20,10 +21,11 @@ import org.springframework.stereotype.Service;
 public class FileServiceImpl implements FileService {
     private final AppDocumentDAO appDocumentDAO;
     private final AppPhotoDAO appPhotoDAO;
+    private final Decoder decoder;
 
     @Override
     public AppDocument getDocument(String codedId) {
-        var id = Long.parseLong(codedId);
+        var id = decoder.decodeId(codedId);
         return appDocumentDAO.findById(id)
                 .orElseThrow(() -> {
                     log.error("Документ с id {} не найден!", id);
@@ -33,7 +35,7 @@ public class FileServiceImpl implements FileService {
 
     @Override
     public AppPhoto getPhoto(String codedId) {
-        var id = Long.parseLong(codedId);
+        var id = decoder.decodeId(codedId);
         return appPhotoDAO.findById(id)
                 .orElseThrow(() -> {
                     log.error("Фото с id {} не найдено!", id);
