2a2baa35e8d157668dafdd9ea87663f12da410fb
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-07-17 23:28:51 +0500

    refactor(node): update rabbit configuration and update producer

--- node/src/main/java/com/github/provitaliy/configuration/RabbitConfig.java
+++ node/src/main/java/com/github/provitaliy/configuration/RabbitConfig.java
@@ -1,10 +1,19 @@
 package com.github.provitaliy.configuration;
 
+import com.github.provitaliy.messaging.ExchangeNames;
+import com.github.provitaliy.messaging.QueueNames;
+import com.github.provitaliy.messaging.RoutingKeys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@RequiredArgsConstructor
 @Configuration
 public class RabbitConfig {
 
@@ -13,4 +22,47 @@ public class RabbitConfig {
         return new Jackson2JsonMessageConverter();
     }
 
+    @Bean
+    public Queue textMessageQueue() {
+        return new Queue(QueueNames.TEXT_MESSAGE_UPDATE_QUEUE);
+    }
+
+    @Bean
+    public Queue docMessageQueue() {
+        return new Queue(QueueNames.DOC_MESSAGE_UPDATE_QUEUE);
+    }
+
+    @Bean
+    public Queue photoMessageQueue() {
+        return new Queue(QueueNames.PHOTO_MESSAGE_UPDATE_QUEUE);
+    }
+
+    @Bean
+    public DirectExchange mainExchange() {
+        return new DirectExchange(ExchangeNames.MAIN);
+    }
+
+    @Bean
+    public Binding textMessageBinding() {
+        return BindingBuilder
+                .bind(textMessageQueue())
+                .to(mainExchange())
+                .with(RoutingKeys.ROUTING_KEY_TEXT_MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Binding docMessageBinding() {
+        return BindingBuilder
+                .bind(docMessageQueue())
+                .to(mainExchange())
+                .with(RoutingKeys.ROUTING_KEY_DOC_MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Binding photoMessageBinding() {
+        return BindingBuilder
+                .bind(photoMessageQueue())
+                .to(mainExchange())
+                .with(RoutingKeys.ROUTING_KEY_PHOTO_MESSAGE_UPDATE);
+    }
 }
--- node/src/main/java/com/github/provitaliy/service/impl/ProducerServiceImpl.java
+++ node/src/main/java/com/github/provitaliy/service/impl/ProducerServiceImpl.java
@@ -1,32 +1,34 @@
 package com.github.provitaliy.service.impl;
 
 import com.github.provitaliy.dto.MailParams;
+import com.github.provitaliy.messaging.ExchangeNames;
+import com.github.provitaliy.messaging.RoutingKeys;
 import com.github.provitaliy.service.ProducerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 @Service
 @RequiredArgsConstructor
 public class ProducerServiceImpl implements ProducerService {
-
-    @Value("${spring.rabbitmq.queues.answer-message}")
-    private String answerQueue;
-
-    @Value("${spring.rabbitmq.queues.registration-mail}")
-    private String emailRegisterQueue;
-
     private final RabbitTemplate rabbitTemplate;
 
     @Override
     public void produceAnswer(SendMessage sendMessage) {
-        rabbitTemplate.convertAndSend(answerQueue, sendMessage);
+        rabbitTemplate.convertAndSend(
+                ExchangeNames.MAIN,
+                RoutingKeys.ROUTING_KEY_ANSWER_MESSAGE,
+                sendMessage
+        );
     }
 
     @Override
     public void produceRegistrationMail(MailParams mailParams) {
-        rabbitTemplate.convertAndSend(emailRegisterQueue, mailParams);
+        rabbitTemplate.convertAndSend(
+                ExchangeNames.MAIN,
+                RoutingKeys.ROUTING_KEY_EMAIL_MESSAGE,
+                mailParams
+        );
     }
 }
