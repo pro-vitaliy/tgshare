a03ef5f1b32776237771d94b66047c34e8e4915b
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-07-17 23:22:53 +0500

    feat(common): update rabbit queues configuration add Exchange names and Routing keys

---
+++ common/src/main/java/com/github/provitaliy/messaging/ExchangeNames.java
@@ -0,0 +1,5 @@
+package com.github.provitaliy.messaging;
+
+public class ExchangeNames {
+    public static final String MAIN = "exchange.main";
+}
---
+++ common/src/main/java/com/github/provitaliy/messaging/RoutingKeys.java
@@ -0,0 +1,9 @@
+package com.github.provitaliy.messaging;
+
+public class RoutingKeys {
+    public static final String ROUTING_KEY_TEXT_MESSAGE_UPDATE = "routing.text";
+    public static final String ROUTING_KEY_DOC_MESSAGE_UPDATE = "routing.document";
+    public static final String ROUTING_KEY_PHOTO_MESSAGE_UPDATE = "routing.photo";
+    public static final String ROUTING_KEY_ANSWER_MESSAGE = "routing.answer";
+    public static final String ROUTING_KEY_EMAIL_MESSAGE = "routing.email";
+}
