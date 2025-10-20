129a3a60e731ab24715b0880c8b0a8dd9b680775
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-10-20 13:33:57 +0500

    refactor(rest-service): merge interfaces and implementations into a single classes

--- rest-service/src/main/java/com/github/provitaliy/service/FileService.java
+++ rest-service/src/main/java/com/github/provitaliy/service/FileService.java
@@ -1,9 +1,43 @@
 package com.github.provitaliy.service;
 
+import com.github.provitaliy.repository.AppDocumentRepository;
+import com.github.provitaliy.repository.AppPhotoRepository;
 import com.github.provitaliy.entity.AppDocument;
 import com.github.provitaliy.entity.AppPhoto;
+import com.github.provitaliy.exception.ResourceNotFoundException;
+import com.github.provitaliy.utils.Decoder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
-public interface FileService {
-    AppDocument getDocument(String codedId);
-    AppPhoto getPhoto(String codedId);
+@Slf4j
+@Service
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class FileService {
+    private final AppDocumentRepository appDocumentRepository;
+    private final AppPhotoRepository appPhotoRepository;
+    private final Decoder decoder;
+
+    public AppDocument getDocument(String codedId) {
+        var id = decoder.decodeId(codedId);
+        return appDocumentRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.error("Документ с id {} не найден!", id);
+                    return new ResourceNotFoundException("Документ не найден!");
+                });
+    }
+
+    public AppPhoto getPhoto(String codedId) {
+        var id = decoder.decodeId(codedId);
+        return appPhotoRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.error("Фото с id {} не найдено!", id);
+                    return new ResourceNotFoundException("Фото не найдено!");
+                });
+    }
 }
+
--- rest-service/src/main/java/com/github/provitaliy/service/UserActivationService.java
+++ rest-service/src/main/java/com/github/provitaliy/service/UserActivationService.java
@@ -1,5 +1,25 @@
 package com.github.provitaliy.service;
 
-public interface UserActivationService {
-    void activateUser(String cryptedUserId);
+import com.github.provitaliy.repository.AppUserRepository;
+import com.github.provitaliy.entity.AppUser;
+import com.github.provitaliy.exception.ResourceNotFoundException;
+import com.github.provitaliy.utils.Decoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserActivationService {
+    private final AppUserRepository appUserRepository;
+    private final Decoder decoder;
+
+    public void activateUser(String encodedUserId) {
+        var userId = decoder.decodeId(encodedUserId);
+        AppUser appUser = appUserRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Такого пользователя не существует."));
+        appUser.setEmail(appUser.getUnconfirmedEmail());
+        appUser.setUnconfirmedEmail(null);
+        appUser.setIsActive(true);
+        appUserRepository.save(appUser);
+    }
 }
