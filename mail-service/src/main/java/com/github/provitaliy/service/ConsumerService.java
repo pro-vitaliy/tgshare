03c5c6a0055d5f56a9bf159b18eb379791675845
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-10-20 13:33:16 +0500

    refactor(mail-service): merge interfaces and implementations into a single classes

--- mail-service/src/main/java/com/github/provitaliy/service/ConsumerService.java
+++ mail-service/src/main/java/com/github/provitaliy/service/ConsumerService.java
@@ -1,7 +1,17 @@
 package com.github.provitaliy.service;
 
 import com.github.provitaliy.dto.MailParams;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
 
-public interface ConsumerService {
-    void consumeRegistrationMail(MailParams mailParams);
+@Service
+@RequiredArgsConstructor
+public class ConsumerService {
+    private final MailSenderService mailSenderService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queues.registration-mail}")
+    public void consumeRegistrationMail(MailParams mailParams) {
+        mailSenderService.send(mailParams);
+    }
 }
