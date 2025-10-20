2a8be2cbe51e2bc78b9329b5d20a0c93d21dcc51
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-10-20 13:30:36 +0500

    refactor(dispatcher): merge AnswerConsumer interface and implementation into a single class

--- dispatcher/src/main/java/com/github/provitaliy/service/AnswerConsumer.java
+++ dispatcher/src/main/java/com/github/provitaliy/service/AnswerConsumer.java
@@ -1,7 +1,21 @@
 package com.github.provitaliy.service;
 
+import com.github.provitaliy.controller.UpdateController;
+import com.github.provitaliy.messaging.QueueNames;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
-public interface AnswerConsumer {
-    void consume(SendMessage sendMessage);
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AnswerConsumer {
+    private final UpdateController updateController;
+
+    @RabbitListener(queues = QueueNames.ANSWER_MESSAGE_QUEUE)
+    public void consume(SendMessage sendMessage) {
+        updateController.setView(sendMessage);
+    }
 }
