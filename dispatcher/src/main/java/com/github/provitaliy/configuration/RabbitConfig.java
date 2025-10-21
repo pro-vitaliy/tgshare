2302fb92d20a3c8305dea858443fd48b21cb1922
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-10-21 22:16:20 +0500

    refactor(dispatcher): configuration rabbit

--- dispatcher/src/main/java/com/github/provitaliy/configuration/RabbitConfig.java
+++ dispatcher/src/main/java/com/github/provitaliy/configuration/RabbitConfig.java
@@ -1,7 +1,13 @@
 package com.github.provitaliy.configuration;
 
+import com.github.provitaliy.messaging.ExchangeNames;
+import com.github.provitaliy.messaging.RoutingKeys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import com.github.provitaliy.messaging.QueueNames;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -18,22 +24,20 @@ public class RabbitConfig {
     }
 
     @Bean
-    public Queue textMessageQueue() {
-        return new Queue(queueProperties.getTextMessageUpdate());
-    }
-
-    @Bean
-    public Queue docMessageQueue() {
-        return new Queue(queueProperties.getDocMessageUpdate());
-    }
-
-    @Bean
-    public Queue photoMessageQueue() {
-        return new Queue(queueProperties.getPhotoMessageUpdate());
-    }
-
-    @Bean
     public Queue answerMessageQueue() {
-        return new Queue(queueProperties.getAnswerMessage());
+        return new Queue(QueueNames.ANSWER_MESSAGE_QUEUE);
+    }
+
+    @Bean
+    public DirectExchange mainExchange() {
+        return new DirectExchange(ExchangeNames.MAIN);
+    }
+
+    @Bean
+    public Binding answerMessageBinding() {
+        return BindingBuilder
+                .bind(answerMessageQueue())
+                .to(mainExchange())
+                .with(RoutingKeys.ROUTING_KEY_ANSWER_MESSAGE);
     }
 }
