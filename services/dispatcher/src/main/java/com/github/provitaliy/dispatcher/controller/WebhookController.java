eb51942f8690e6e61912b2779bba58ef9d85e537
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-10-25 00:18:18 +0500

    dispatcher: add WebhookController

---
+++ services/dispatcher/src/main/java/com/github/provitaliy/dispatcher/controller/WebhookController.java
@@ -0,0 +1,19 @@
+package com.github.provitaliy.dispatcher.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@RequiredArgsConstructor
+@RestController
+public class WebhookController {
+
+    private final UpdateController updateController;
+
+    @PostMapping("/")
+    public void onUpdatesReceived(@RequestBody Update update) {
+
+    }
+}
