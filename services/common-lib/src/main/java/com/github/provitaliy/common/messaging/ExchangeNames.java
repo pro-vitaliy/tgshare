5ec4027c41c9d220fb5852a4eb28fe39f1a2b463
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-10-24 22:42:20 +0500

    common-lib: add Rabbit constants: queue names, exchange names, routing keys

---
+++ services/common-lib/src/main/java/com/github/provitaliy/common/messaging/ExchangeNames.java
@@ -0,0 +1,5 @@
+package com.github.provitaliy.common.messaging;
+
+public class ExchangeNames {
+    public static final String MAIN = "exchange.main";
+}
---
+++ services/common-lib/src/main/java/com/github/provitaliy/common/messaging/QueueNames.java
@@ -0,0 +1,9 @@
+package com.github.provitaliy.common.messaging;
+
+public class QueueNames {
+    public static final String TEXT_MESSAGE_UPDATE_QUEUE = "rabbit.text.message.update.queue";
+    public static final String DOC_MESSAGE_UPDATE_QUEUE = "rabbit.doc.message.update.queue";
+    public static final String PHOTO_MESSAGE_UPDATE_QUEUE = "rabbit.photo.message.update.queue";
+    public static final String ANSWER_MESSAGE_QUEUE = "rabbit.answer.message.queue";
+    public static final String REGISTRATION_MAIL_QUEUE = "rabbit.registration.mail.queue";
+}
