04a5d4a20f238f25e3c49844f9e831645d41c03b
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-01 13:12:56 +0500

    user-service: add RabbitConfig for message handling

---
+++ services/user-service/src/main/java/com/github/provitaliy/userservice/config/RabbitConfig.java
@@ -0,0 +1,40 @@
+package com.github.provitaliy.userservice.config;
+
+import com.github.provitaliy.common.messaging.ExchangeNames;
+import com.github.provitaliy.common.messaging.QueueNames;
+import com.github.provitaliy.common.messaging.RoutingKeys;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue userEmailEnteredQueue() {
+        return new Queue(QueueNames.USER_EMAIL_ENTERED_QUEUE);
+    }
+
+    @Bean
+    public DirectExchange mainExchange() {
+        return new DirectExchange(ExchangeNames.MAIN);
+    }
+
+    @Bean
+    public Binding userEmailEnteredBinding() {
+        return BindingBuilder
+                .bind(userEmailEnteredQueue())
+                .to(mainExchange())
+                .with(RoutingKeys.ROUTING_KEY_USER_EMAIL_ENTERED);
+    }
+}
