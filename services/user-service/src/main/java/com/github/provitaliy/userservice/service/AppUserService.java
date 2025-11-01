36cfd1fc106b9b5fdb730c862fc7d11300e7e086
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-01 15:47:40 +0500

    user-service: restore stable working state after reset

--- services/user-service/src/main/java/com/github/provitaliy/userservice/service/AppUserService.java
+++ services/user-service/src/main/java/com/github/provitaliy/userservice/service/AppUserService.java
@@ -7,6 +7,7 @@ import com.github.provitaliy.userservice.exception.EmailAlreadyTakenException;
 import com.github.provitaliy.userservice.exception.UserNotFoundException;
 import com.github.provitaliy.userservice.mapper.AppUserMapper;
 import com.github.provitaliy.userservice.repository.AppUserRepository;
+import com.github.provitaliy.userservice.service.email.EmailConfirmationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 public class AppUserService {
     private final AppUserRepository appUserRepository;
     private final AppUserMapper appUserMapper;
+    private final EmailConfirmationService confirmationService;
 
     public AppUserDTO getOrCreateAppUser(AppUserCreateDTO appUserData) {
         return appUserRepository.findByTelegramUserId(appUserData.getTelegramUserId())
@@ -27,16 +29,26 @@ public class AppUserService {
                 });
     }
 
-    public void updateUnconfirmedEmail(Long telegramUserId, String email) {
+    public void updateUnconfirmedEmail(Long userId, String email) {
         if (appUserRepository.existsByEmail(email)) {
+
+//            TODO: не забыть сделать обработчики этих исколючений в ноде
+
             throw new EmailAlreadyTakenException(email);
         }
 
-        AppUser appUser = appUserRepository.findByTelegramUserId(telegramUserId)
-                .orElseThrow(() -> new UserNotFoundException(telegramUserId));
+        AppUser appUser = appUserRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
 
         appUser.setUnconfirmedEmail(email);
-//        TODO: здесь будет отправка ссылки с подтверждением на почту
-//        TODO: здесь будет логика публикации обновленного юзера (хотя если почта не потверждена то и не надо мб)
+        appUserRepository.save(appUser);
+        confirmationService.sendConfirmationEmail(userId, email);
+    }
+
+    public void activateUser(Long id) {
+        AppUser user = appUserRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+
+        user.setIsActive(true);
     }
 }
