ba01196621ca72889f23bdb93ce5d1ef795c1284
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-01 13:36:19 +0500

    user-service: remove updateUnconfirmedEmail method from AppUserGrpcService

--- services/user-service/src/main/java/com/github/provitaliy/userservice/grpc/AppUserGrpcService.java
+++ services/user-service/src/main/java/com/github/provitaliy/userservice/grpc/AppUserGrpcService.java
@@ -5,13 +5,8 @@ import com.github.provitaliy.common.dto.AppUserDTO;
 import com.github.provitaliy.common.grpc.AppUserResponse;
 import com.github.provitaliy.common.grpc.AppUserServiceGrpc;
 import com.github.provitaliy.common.grpc.GetOrCreateAppUserRequest;
-import com.github.provitaliy.common.grpc.UpdateUnconfirmedEmailRequest;
-import com.github.provitaliy.userservice.exception.EmailAlreadyTakenException;
-import com.github.provitaliy.userservice.exception.UserNotFoundException;
 import com.github.provitaliy.userservice.mapper.AppUserGrpcMapper;
 import com.github.provitaliy.userservice.service.AppUserService;
-import com.google.protobuf.Empty;
-import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.grpc.server.service.GrpcService;
@@ -31,21 +26,5 @@ public class AppUserGrpcService extends AppUserServiceGrpc.AppUserServiceImplBas
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
-    }
-
-    @Override
-    public void updateUnconfirmedEmail(UpdateUnconfirmedEmailRequest request,
-                                       StreamObserver<Empty> responseObserver) {
-        try {
-            appUserService.updateUnconfirmedEmail(request.getTelegramUserId(), request.getEmail());
-            responseObserver.onNext(Empty.getDefaultInstance());
-            responseObserver.onCompleted();
-        } catch (EmailAlreadyTakenException e) {
-            responseObserver.onError(Status.ALREADY_EXISTS.withDescription(e.getMessage()).asRuntimeException());
-        } catch (UserNotFoundException e) {
-            responseObserver.onError(Status.NOT_FOUND.withDescription(e.getMessage()).asRuntimeException());
-        } catch (Exception e) {
-            responseObserver.onError(Status.INTERNAL.withDescription("Unexpected error").asRuntimeException());
-        }
     }
 }
