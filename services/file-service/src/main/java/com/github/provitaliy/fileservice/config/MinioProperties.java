3ddcea8627b741fdc007dfe7faa1b67ed2d99ad0
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-03 23:33:26 +0500

    file-service: add MinioProperties class for MinIO configuration properties

---
+++ services/file-service/src/main/java/com/github/provitaliy/fileservice/config/MinioProperties.java
@@ -0,0 +1,17 @@
+package com.github.provitaliy.fileservice.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@ConfigurationProperties(prefix = "minio")
+@Component
+public class MinioProperties {
+    private String url;
+    private String accessKey;
+    private String secretKey;
+    private String bucket;
+}
