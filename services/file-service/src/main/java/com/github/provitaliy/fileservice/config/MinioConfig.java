4fc7af43fb23bae1cd59dc7aee2fd3374d993eb1
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-03 22:38:19 +0500

    file-service: add MinioConfig class for MinIO client configuration

---
+++ services/file-service/src/main/java/com/github/provitaliy/fileservice/config/MinioConfig.java
@@ -0,0 +1,26 @@
+package com.github.provitaliy.fileservice.config;
+
+import io.minio.MinioClient;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Setter
+@Getter
+@ConfigurationProperties(prefix = "minio")
+@Configuration
+public class MinioConfig {
+    private String url;
+    private String accessKey;
+    private String secretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
