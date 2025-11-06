16e79531e253edb695a49c2ab8be59ec52af4c0c
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-06 22:50:36 +0500

    file-service: add FileServiceProperties

---
+++ services/file-service/src/main/java/com/github/provitaliy/fileservice/config/FileServiceProperties.java
@@ -0,0 +1,16 @@
+package com.github.provitaliy.fileservice.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@ConfigurationProperties(prefix = "file-service")
+@Component
+public class FileServiceProperties {
+    private String host;
+    private Integer port;
+    private String downloadEndpoint;
+}
