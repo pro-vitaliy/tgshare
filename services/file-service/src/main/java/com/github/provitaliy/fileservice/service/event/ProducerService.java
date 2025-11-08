ae63f1f8c54a5f0d4d575f468575cf1ef0d9829f
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-08 15:13:51 +0500

    file-service: add ProducerService for publishing FileReadyEvent

---
+++ services/file-service/src/main/java/com/github/provitaliy/fileservice/service/event/ProducerService.java
@@ -0,0 +1,22 @@
+package com.github.provitaliy.fileservice.service.event;
+
+import com.github.provitaliy.common.event.FileReadyEvent;
+import com.github.provitaliy.common.messaging.ExchangeNames;
+import com.github.provitaliy.common.messaging.RoutingKeys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ProducerService {
+    private final RabbitTemplate rabbitTemplate;
+
+    public void produceFileReadyEvent(FileReadyEvent readyEvent) {
+        rabbitTemplate.convertAndSend(
+                ExchangeNames.MAIN,
+                RoutingKeys.ROUTING_KEY_FILE_READY,
+                readyEvent
+        );
+    }
+}
