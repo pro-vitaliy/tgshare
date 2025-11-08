770f03ffed968c8d297070ca73f0a4c81ab4e0b9
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-08 15:22:16 +0500

    common-lib: add @Builder to AppUserCreateDTO

--- services/common-lib/src/main/java/com/github/provitaliy/common/dto/AppUserCreateDTO.java
+++ services/common-lib/src/main/java/com/github/provitaliy/common/dto/AppUserCreateDTO.java
@@ -1,10 +1,12 @@
 package com.github.provitaliy.common.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Builder
 @Setter
 @Getter
 @AllArgsConstructor
