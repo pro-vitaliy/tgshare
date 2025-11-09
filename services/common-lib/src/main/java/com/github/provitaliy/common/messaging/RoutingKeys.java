7d4d3c4fb1cf5e79664fd17d15de8d9c1371cdf2
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-09 12:12:43 +0500

    common-lib: Add routing key for User Activated event

--- services/common-lib/src/main/java/com/github/provitaliy/common/messaging/RoutingKeys.java
+++ services/common-lib/src/main/java/com/github/provitaliy/common/messaging/RoutingKeys.java
@@ -9,5 +9,5 @@ public class RoutingKeys {
     public static final String ROUTING_KEY_EMAIL_SEND = "routing.email.send";
     public static final String ROUTING_KEY_FILE_UPLOAD_REQUEST = "routing.file.upload.request";
     public static final String ROUTING_KEY_FILE_READY = "routing.file.ready";
-
+    public static final String ROUTING_KEY_USER_ACTIVATED = "routing.user.activated";
 }
