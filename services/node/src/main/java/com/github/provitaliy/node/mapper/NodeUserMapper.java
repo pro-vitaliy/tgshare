58e66a0e9117507e6285b26cfacbbe07a46367af
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-10 23:09:38 +0500

    node: replace MapStruct with manual mapping in NodeUserMapper

--- services/node/src/main/java/com/github/provitaliy/node/mapper/NodeUserMapper.java
+++ services/node/src/main/java/com/github/provitaliy/node/mapper/NodeUserMapper.java
@@ -4,17 +4,31 @@ import com.github.provitaliy.common.dto.AppUserCreateDTO;
 import com.github.provitaliy.common.grpc.AppUserResponse;
 import com.github.provitaliy.common.grpc.GetOrCreateAppUserRequest;
 import com.github.provitaliy.node.user.NodeUser;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
+import com.github.provitaliy.node.user.UserState;
+import org.springframework.stereotype.Component;
 
-@Mapper(
-        componentModel = MappingConstants.ComponentModel.SPRING,
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
-        unmappedTargetPolicy = ReportingPolicy.IGNORE
-)
-public abstract class NodeUserMapper {
-    public abstract GetOrCreateAppUserRequest toGrpc(AppUserCreateDTO userCreateDto);
-    public abstract NodeUser fromGrpc(AppUserResponse appUserResponse);
+@Component
+public class NodeUserMapper {
+    public GetOrCreateAppUserRequest toGrpc(AppUserCreateDTO userDto) {
+        return GetOrCreateAppUserRequest.newBuilder()
+                .setTelegramUserId(userDto.getTelegramUserId())
+                .setChatId(userDto.getChatId())
+                .setFirstName(userDto.getFirstName() == null ? "" : userDto.getFirstName())
+                .setLastName(userDto.getLastName() == null ? "" : userDto.getLastName())
+                .setUsername(userDto.getUsername() == null ? "" : userDto.getUsername())
+                .build();
+    }
+
+    public NodeUser fromGrpc(AppUserResponse appUserResponse) {
+        return NodeUser.builder()
+                .telegramUserId(appUserResponse.getTelegramUserId())
+                .chatId(appUserResponse.getChatId())
+                .firstName(appUserResponse.getFirstName())
+                .lastName(appUserResponse.getLastName())
+                .username(appUserResponse.getUsername())
+                .email(appUserResponse.getEmail())
+                .isActive(appUserResponse.getIsActive())
+                .state(UserState.BASIC_STATE)
+                .build();
+    }
 }
