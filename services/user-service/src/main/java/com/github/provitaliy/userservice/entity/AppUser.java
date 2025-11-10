3e5c8af048e5efdb429677fd23171c89a7713853
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-10 23:00:58 +0500

    user-service: add no-args and all-args constructors to AppUser

--- services/user-service/src/main/java/com/github/provitaliy/userservice/entity/AppUser.java
+++ services/user-service/src/main/java/com/github/provitaliy/userservice/entity/AppUser.java
@@ -5,8 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -15,6 +17,8 @@ import java.time.LocalDateTime;
 @Builder
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "app_user")
 @Entity
 public class AppUser {
