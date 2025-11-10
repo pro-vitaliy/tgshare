4be1978cfea065b9201704fd940237d640b5abcb
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-10 23:12:42 +0500

    node: add no-args and all-args constructors to NodeUser

--- services/node/src/main/java/com/github/provitaliy/node/user/NodeUser.java
+++ services/node/src/main/java/com/github/provitaliy/node/user/NodeUser.java
@@ -1,10 +1,14 @@
 package com.github.provitaliy.node.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class NodeUser {
