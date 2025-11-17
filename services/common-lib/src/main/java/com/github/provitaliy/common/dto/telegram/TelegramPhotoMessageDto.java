a84a39be0eba056fafaa49b1d2aa61ee09a2d9c3
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-17 23:48:15 +0500

    common-lib: add TelegramPhotoMessageDto record for Telegram photo message representation

---
+++ services/common-lib/src/main/java/com/github/provitaliy/common/dto/telegram/TelegramPhotoMessageDto.java
@@ -0,0 +1,9 @@
+package com.github.provitaliy.common.dto.telegram;
+
+public record TelegramPhotoMessageDto(
+        Long chatId,
+        TelegramUserDto from,
+        String photoId,
+        String photoName,
+        Long photoSize
+) { }
