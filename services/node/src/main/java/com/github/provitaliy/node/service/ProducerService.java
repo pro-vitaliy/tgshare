5d844472aa393796747db860e20ef81ee0a3d1f7
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-20 23:49:16 +0500

    node-service: refactor ProducerService to use SendMessageDto for message production

--- services/node/src/main/java/com/github/provitaliy/node/service/ProducerService.java
+++ services/node/src/main/java/com/github/provitaliy/node/service/ProducerService.java
@@ -1,5 +1,6 @@
 package com.github.provitaliy.node.service;
 
+import com.github.provitaliy.common.dto.telegram.SendMessageDto;
 import com.github.provitaliy.common.event.FileUploadEvent;
 import com.github.provitaliy.common.event.UserEmailEnteredEvent;
 import com.github.provitaliy.common.messaging.ExchangeNames;
@@ -7,14 +8,13 @@ import com.github.provitaliy.common.messaging.RoutingKeys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 @RequiredArgsConstructor
 @Service
 public class ProducerService {
     private final RabbitTemplate rabbitTemplate;
 
-    public void produceAnswer(SendMessage sendMessage) {
+    public void produceAnswer(SendMessageDto sendMessage) {
         rabbitTemplate.convertAndSend(
                 ExchangeNames.MAIN,
                 RoutingKeys.ROUTING_KEY_ANSWER_MESSAGE,
