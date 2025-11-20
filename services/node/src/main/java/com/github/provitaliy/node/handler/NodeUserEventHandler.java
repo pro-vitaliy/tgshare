1ddf549ddf412afb691fc9973d579af98696165a
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-20 23:48:38 +0500

    node-service: refactor NodeUserEventHandler to use SendMessageDto for message preparation

--- services/node/src/main/java/com/github/provitaliy/node/handler/NodeUserEventHandler.java
+++ services/node/src/main/java/com/github/provitaliy/node/handler/NodeUserEventHandler.java
@@ -1,12 +1,12 @@
 package com.github.provitaliy.node.handler;
 
+import com.github.provitaliy.common.dto.telegram.SendMessageDto;
 import com.github.provitaliy.common.event.UserActivatedEvent;
 import com.github.provitaliy.node.service.NodeUserService;
 import com.github.provitaliy.node.service.ProducerService;
 import com.github.provitaliy.node.user.NodeUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 @RequiredArgsConstructor
 @Service
@@ -21,7 +21,7 @@ public class NodeUserEventHandler {
         userService.updateNodeUser(nodeUser);
 
         String messageText = "Ваш email успешно подтвержден, учетная запись активирована!";
-        SendMessage message = HandlerUtils.prepareMessage(messageText, nodeUser.getChatId());
+        SendMessageDto message = HandlerUtils.prepareSendMessage(messageText, nodeUser.getChatId());
         producerService.produceAnswer(message);
     }
 }
