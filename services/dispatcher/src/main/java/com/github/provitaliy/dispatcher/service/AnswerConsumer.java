4f144bd8b87c5643524d517e9a24a2f431da19d7
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-20 22:52:58 +0500

    dispatcher: refactor AnswerConsumer to use SendMessageDto for message consumption

--- services/dispatcher/src/main/java/com/github/provitaliy/dispatcher/service/AnswerConsumer.java
+++ services/dispatcher/src/main/java/com/github/provitaliy/dispatcher/service/AnswerConsumer.java
@@ -1,20 +1,19 @@
 package com.github.provitaliy.dispatcher.service;
 
+import com.github.provitaliy.common.dto.telegram.SendMessageDto;
 import com.github.provitaliy.common.messaging.QueueNames;
 import com.github.provitaliy.dispatcher.controller.UpdateController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 @RequiredArgsConstructor
 @Service
 public class AnswerConsumer {
-
     private final UpdateController updateController;
 
     @RabbitListener(queues = QueueNames.ANSWER_MESSAGE_QUEUE)
-    public void consume(SendMessage sendMessage) {
-        updateController.setView(sendMessage);
+    public void consume(SendMessageDto sendMessageDto) {
+        updateController.setView(sendMessageDto);
     }
 }
