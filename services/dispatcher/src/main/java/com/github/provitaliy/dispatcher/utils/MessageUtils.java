66feccf2d560b79742c661d49a8cd2dd21836f3d
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-21 20:53:37 +0500

    dispatcher: refactor MessageUtils to use UtilityClass annotation and make generateSendMessageWithText static

--- services/dispatcher/src/main/java/com/github/provitaliy/dispatcher/utils/MessageUtils.java
+++ services/dispatcher/src/main/java/com/github/provitaliy/dispatcher/utils/MessageUtils.java
@@ -1,13 +1,13 @@
 package com.github.provitaliy.dispatcher.utils;
 
-import org.springframework.stereotype.Component;
+import lombok.experimental.UtilityClass;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-@Component
+@UtilityClass
 public class MessageUtils {
 
-    public SendMessage generateSendMessageWithText(Update update, String text) {
+    public static SendMessage generateSendMessageWithText(Update update, String text) {
         var message = update.getMessage();
         var chatId = message.getChatId();
         return SendMessage.builder()
