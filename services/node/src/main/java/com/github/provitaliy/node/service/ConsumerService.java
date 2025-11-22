b9194fe4fe64b64e71a872546950d5effaa6d468
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-22 23:25:28 +0500

    node-service: refactor ConsumerService to use UserDomainEventHandler and add TelegramMessageListener for message consumption

--- services/node/src/main/java/com/github/provitaliy/node/service/ConsumerService.java
+++ services/node/src/main/java/com/github/provitaliy/node/service/ConsumerService.java
@@ -6,10 +6,10 @@ import com.github.provitaliy.common.dto.telegram.TelegramTextMessageDto;
 import com.github.provitaliy.common.event.FileReadyEvent;
 import com.github.provitaliy.common.event.UserActivatedEvent;
 import com.github.provitaliy.common.messaging.QueueNames;
+import com.github.provitaliy.node.aspect.TelegramMessageListener;
 import com.github.provitaliy.node.handler.FileUpdateHandler;
-import com.github.provitaliy.node.handler.NodeUserEventHandler;
+import com.github.provitaliy.node.handler.UserDomainEventHandler;
 import com.github.provitaliy.node.handler.TextUpdateHandler;
-import com.github.provitaliy.node.handler.UserNotificationEventHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -21,21 +21,23 @@ import org.springframework.stereotype.Service;
 public class ConsumerService {
     private final TextUpdateHandler textUpdateHandler;
     private final FileUpdateHandler fileUpdateHandler;
-    private final UserNotificationEventHandler userNotificationEventHandler;
-    private final NodeUserEventHandler userEventHandler;
+    private final UserDomainEventHandler userDomainEventHandler;
 
+    @TelegramMessageListener
     @RabbitListener(queues = QueueNames.TEXT_MESSAGE_UPDATE_QUEUE)
     public void consumeTextMessageUpdates(TelegramTextMessageDto textMessage) {
         log.debug("NODE: Text message is received");
         textUpdateHandler.handleUpdate(textMessage);
     }
 
+    @TelegramMessageListener
     @RabbitListener(queues = QueueNames.DOC_MESSAGE_UPDATE_QUEUE)
     public void consumeDocMessageUpdates(TelegramDocumentMessageDto docMessage) {
         log.debug("NODE: Doc message is received");
         fileUpdateHandler.handleDocUpdate(docMessage);
     }
 
+    @TelegramMessageListener
     @RabbitListener(queues = QueueNames.PHOTO_MESSAGE_UPDATE_QUEUE)
     public void consumePhotoMessageUpdates(TelegramPhotoMessageDto photoMessage) {
         log.debug("NODE: Photo message is received");
@@ -44,12 +46,11 @@ public class ConsumerService {
 
     @RabbitListener(queues = QueueNames.FILE_READY_QUEUE)
     public void consumeFileReadyEvent(FileReadyEvent event) {
-        userNotificationEventHandler.handleFileReadyEvent(event.telegramUserId(), event.fileUrl());
+        userDomainEventHandler.handle(event);
     }
 
     @RabbitListener(queues = QueueNames.USER_ACTIVATED_QUEUE)
     public void consumeUserActivationEvent(UserActivatedEvent event) {
-        userEventHandler.userActivatedEventHandler(event);
-//        TODO: опубликовать возможные исключение в DLQ
+        userDomainEventHandler.handle(event);
     }
 }
