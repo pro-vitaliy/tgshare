198a3399c6124f34e647dc17c9bc397d0080168b
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-22 23:23:48 +0500

    node-service: enhance NodeUserCacheService with error handling for Redis access

--- services/node/src/main/java/com/github/provitaliy/node/service/NodeUserCacheService.java
+++ services/node/src/main/java/com/github/provitaliy/node/service/NodeUserCacheService.java
@@ -2,11 +2,13 @@ package com.github.provitaliy.node.service;
 
 import com.github.provitaliy.node.user.NodeUser;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class NodeUserCacheService {
@@ -18,7 +20,13 @@ public class NodeUserCacheService {
     }
 
     public Optional<NodeUser> findByTelegramId(Long telegramUserId) {
-        return Optional.ofNullable(redisTemplate.opsForValue().get(PREFIX + telegramUserId));
+        try {
+            return Optional.ofNullable(redisTemplate.opsForValue().get(PREFIX + telegramUserId));
+        } catch (Exception e) {
+            // TODO: send alert/metric: Redis is unavailable
+            log.error("Redis unavailable", e);
+            return Optional.empty();
+        }
     }
 
     public void delete(long telegramId) {
