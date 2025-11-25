11bd369b34f28bdcc5de05b09071747707be537a
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-25 22:50:20 +0500

    common-lib: add dead-letter queue exchange name to ExchangeNames

--- services/common-lib/src/main/java/com/github/provitaliy/common/messaging/ExchangeNames.java
+++ services/common-lib/src/main/java/com/github/provitaliy/common/messaging/ExchangeNames.java
@@ -2,4 +2,5 @@ package com.github.provitaliy.common.messaging;
 
 public class ExchangeNames {
     public static final String MAIN = "exchange.main";
+    public static final String DLQ = "exchange.dlq";
 }
