4d6b3ee0ab433e6ed1f8675ce3a825e594f09674
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-29 21:31:29 +0500

    user-service: remove unused  EmailAlreadyTakenException

--- services/user-service/src/main/java/com/github/provitaliy/userservice/exception/EmailAlreadyTakenException.java
+++
@@ -1,8 +0,0 @@
-package com.github.provitaliy.userservice.exception;
-
-public class EmailAlreadyTakenException extends RuntimeException {
-
-    public EmailAlreadyTakenException(String email) {
-        super("Email already taken: " + email);
-    }
-}
