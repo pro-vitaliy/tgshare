1f4a8f41f0338c1498d524b0e8ae649adeed01f6
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-29 21:55:30 +0500

    common-lib: add DLQ and new queue/routing key for EmailAlreadyTakenEvent

--- services/common-lib/src/main/java/com/github/provitaliy/common/messaging/QueueNames.java
+++ services/common-lib/src/main/java/com/github/provitaliy/common/messaging/QueueNames.java
@@ -4,8 +4,13 @@ public class QueueNames {
     public static final String TEXT_MESSAGE_UPDATE_QUEUE = "rabbit.text.message.update.queue";
     public static final String DOC_MESSAGE_UPDATE_QUEUE = "rabbit.doc.message.update.queue";
     public static final String PHOTO_MESSAGE_UPDATE_QUEUE = "rabbit.photo.message.update.queue";
+
     public static final String ANSWER_MESSAGE_QUEUE = "rabbit.answer.message.queue";
+
     public static final String USER_EMAIL_ENTERED_QUEUE = "rabbit.user.email.entered.queue";
+    public static final String USER_EMAIL_ENTERED_DLQ = "rabbit.user.email.entered.dlq";
+    public static final String EMAIL_ALREADY_TAKEN_QUEUE = "rabbit.email.already.taken.queue";
+
     public static final String EMAIL_SEND_QUEUE = "rabbit.email.send.queue";
     public static final String FILE_UPLOAD_REQUEST_QUEUE = "rabbit.file.upload.request.queue";
 
--- services/common-lib/src/main/java/com/github/provitaliy/common/messaging/RoutingKeys.java
+++ services/common-lib/src/main/java/com/github/provitaliy/common/messaging/RoutingKeys.java
@@ -4,9 +4,15 @@ public class RoutingKeys {
     public static final String ROUTING_KEY_TEXT_MESSAGE_UPDATE = "routing.text";
     public static final String ROUTING_KEY_DOC_MESSAGE_UPDATE = "routing.document";
     public static final String ROUTING_KEY_PHOTO_MESSAGE_UPDATE = "routing.photo";
+
     public static final String ROUTING_KEY_ANSWER_MESSAGE = "routing.answer";
+
     public static final String ROUTING_KEY_USER_EMAIL_ENTERED = "routing.user.email.entered";
+    public static final String ROUTING_KEY_USER_EMAIL_ENTERED_DLQ = "routing.user.email.entered.dlq";
+    public static final String ROUTING_KEY_EMAIL_ALREADY_TAKEN = "routing.email.already.taken";
+
     public static final String ROUTING_KEY_EMAIL_SEND = "routing.email.send";
+
     public static final String ROUTING_KEY_FILE_UPLOAD_REQUEST = "routing.file.upload.request";
 
     public static final String ROUTING_KEY_FILE_READY = "routing.file.ready";
