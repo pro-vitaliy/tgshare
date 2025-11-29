e40d151bc2ba5217dc040c9711ff78199406ec1f
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-29 22:50:33 +0500

    user-service: add handler for EmailAlreadyTakenEvent to notify users

--- services/node/src/main/java/com/github/provitaliy/node/handler/UserDomainEventHandler.java
+++ services/node/src/main/java/com/github/provitaliy/node/handler/UserDomainEventHandler.java
@@ -1,10 +1,10 @@
 package com.github.provitaliy.node.handler;
 
+import com.github.provitaliy.common.event.EmailAlreadyTakenEvent;
 import com.github.provitaliy.common.event.FileReadyEvent;
 import com.github.provitaliy.common.event.UserActivatedEvent;
 import com.github.provitaliy.node.service.NodeUserCacheService;
 import com.github.provitaliy.node.service.NodeUserService;
-import com.github.provitaliy.node.service.ProducerService;
 import com.github.provitaliy.node.service.UserResponseService;
 import com.github.provitaliy.node.user.NodeUser;
 import lombok.RequiredArgsConstructor;
@@ -30,6 +30,12 @@ public class UserDomainEventHandler {
     public void handle(FileReadyEvent fileReadyEvent) {
         String messageText = "Файл успешно, загружен и доступен по ссылке: %s".formatted(fileReadyEvent.fileUrl());
         NodeUser user = userService.getByTelegramUserId(fileReadyEvent.telegramUserId());
+        userResponseService.sendUserResponse(user.getChatId(), messageText);
+    }
+
+    public void handle(EmailAlreadyTakenEvent event) {
+        NodeUser user = userService.getByTelegramUserId(event.telegramUserId());
+        String messageText = "Email %s уже занят. Пожалуйста, используйте другой email.".formatted(event.email());
         userResponseService.sendUserResponse(user.getChatId(), messageText);
     }
 }
