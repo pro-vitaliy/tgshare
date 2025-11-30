e1fae952ba9609fc18dce9c2735782ec4040de76
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-30 23:41:20 +0500

    file-service: add produceFileUploadFailedEvent method to handle file upload failure events

--- services/file-service/src/main/java/com/github/provitaliy/fileservice/service/event/ProducerService.java
+++ services/file-service/src/main/java/com/github/provitaliy/fileservice/service/event/ProducerService.java
@@ -1,6 +1,7 @@
 package com.github.provitaliy.fileservice.service.event;
 
 import com.github.provitaliy.common.event.FileReadyEvent;
+import com.github.provitaliy.common.event.FileUploadFailedEvent;
 import com.github.provitaliy.common.messaging.ExchangeNames;
 import com.github.provitaliy.common.messaging.RoutingKeys;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,14 @@ public class ProducerService {
                 ExchangeNames.MAIN,
                 RoutingKeys.ROUTING_KEY_FILE_READY,
                 readyEvent
+        );
+    }
+
+    public void produceFileUploadFailedEvent(FileUploadFailedEvent failedEvent) {
+        rabbitTemplate.convertAndSend(
+                ExchangeNames.MAIN,
+                RoutingKeys.ROUTING_KEY_FILE_UPLOAD_FAILED,
+                failedEvent
         );
     }
 }
