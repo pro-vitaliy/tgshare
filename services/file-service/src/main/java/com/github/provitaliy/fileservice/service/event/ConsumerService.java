a7e73a853ae1a3fd62b465b5220316b039516645
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-11-30 23:36:36 +0500

    file-service: add RabbitListener for file upload requests

--- services/file-service/src/main/java/com/github/provitaliy/fileservice/service/event/ConsumerService.java
+++ services/file-service/src/main/java/com/github/provitaliy/fileservice/service/event/ConsumerService.java
@@ -1,8 +1,10 @@
 package com.github.provitaliy.fileservice.service.event;
 
 import com.github.provitaliy.common.event.FileUploadEvent;
+import com.github.provitaliy.common.messaging.QueueNames;
 import com.github.provitaliy.fileservice.service.file.FileProcessingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class ConsumerService {
     private final FileProcessingService processingService;
 
+    @RabbitListener(queues = QueueNames.FILE_UPLOAD_REQUEST_QUEUE)
     public void consumeFileUploadRequest(FileUploadEvent uploadEvent) {
         processingService.handleFileUpload(uploadEvent);
     }
