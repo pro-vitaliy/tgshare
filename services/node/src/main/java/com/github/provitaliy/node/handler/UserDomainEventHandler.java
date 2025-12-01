6585ffbe8f99031d20bf8d24544321a6c26f6303
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-12-01 23:34:35 +0500

    node-service: add handler for FileUploadFailedEvent to notify users of upload failures

--- services/node/src/main/java/com/github/provitaliy/node/handler/UserDomainEventHandler.java
+++ services/node/src/main/java/com/github/provitaliy/node/handler/UserDomainEventHandler.java
@@ -2,6 +2,7 @@ package com.github.provitaliy.node.handler;
 
 import com.github.provitaliy.common.event.EmailAlreadyTakenEvent;
 import com.github.provitaliy.common.event.FileReadyEvent;
+import com.github.provitaliy.common.event.FileUploadFailedEvent;
 import com.github.provitaliy.common.event.UserActivatedEvent;
 import com.github.provitaliy.node.service.NodeUserCacheService;
 import com.github.provitaliy.node.service.NodeUserService;
@@ -36,6 +37,12 @@ public class UserDomainEventHandler {
     public void handle(EmailAlreadyTakenEvent event) {
         NodeUser user = userService.getByTelegramUserId(event.telegramUserId());
         String messageText = "Email %s уже занят. Пожалуйста, используйте другой email.".formatted(event.email());
+        userResponseService.sendUserResponse(user.getChatId(), messageText);
+    }
+
+    public void handle(FileUploadFailedEvent event) {
+        NodeUser user = userService.getByTelegramUserId(event.telegramUserId());
+        String messageText = "Не удалось загрузить файл %s. Пожалуйста, попробуйте еще раз.".formatted(event.fileName());
         userResponseService.sendUserResponse(user.getChatId(), messageText);
     }
 }
