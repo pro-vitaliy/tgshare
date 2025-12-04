6e33a9ee8d913cdd3fa23e926a9bb1cc8af95236
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-12-04 23:53:13 +0500

    file-service: enhance TelegramDownloadFileService to use CloseableHttpClient for file downloads

--- services/file-service/src/main/java/com/github/provitaliy/fileservice/service/file/TelegramDownloadFileService.java
+++ services/file-service/src/main/java/com/github/provitaliy/fileservice/service/file/TelegramDownloadFileService.java
@@ -5,6 +5,9 @@ import com.github.provitaliy.fileservice.config.TelegramProperties;
 import com.github.provitaliy.fileservice.exception.DownloadFileException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
@@ -12,29 +15,41 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class TelegramDownloadFileService {
+    private final CloseableHttpClient httpClient;
     private final RestClient restClient;
     private final TelegramProperties props;
 
     @Retryable(
-            retryFor = RestClientException.class,
+            retryFor = {RestClientException.class, IOException.class},
             maxAttempts = 3,
             backoff = @Backoff(delay = 2000)
     )
-    public InputStream downloadFileById(String telegramFileId) {
+    public InputStream downloadFileById(String telegramFileId) throws IOException {
         String filePath = fetchFilePath(telegramFileId);
         String storageUrl = props.getFileStorageUrl()
                 .replace("{token}", props.getBotToken())
                 .replace("{filePath}", filePath);
 
-        return restClient.get()
-                .uri(storageUrl)
-                .exchange((request, response) -> response.getBody());
+        CloseableHttpResponse response = httpClient.execute(new HttpGet(storageUrl));
+
+        if (response.getStatusLine().getStatusCode() != 200) {
+            log.error("Failed to download file from Telegram, status: {}", response.getStatusLine());
+            throw new DownloadFileException("Failed to download file from Telegram");
+        }
+        return response.getEntity().getContent();
+    }
+
+    @Recover
+    public InputStream recover(Exception e, String fileId) {
+        log.error("Failed to download Telegram file after retries, fileId={}", fileId, e);
+        throw new DownloadFileException("Failed to download file from Telegram after retries", e);
     }
 
     private String fetchFilePath(String fileId) {
@@ -55,11 +70,5 @@ public class TelegramDownloadFileService {
         return fileInfo.path("result")
                 .path("file_path")
                 .asText();
-    }
-
-    @Recover
-    public InputStream recover(RestClientException e, String fileId) {
-        log.error("Failed to download Telegram file after retries, fileId={}", fileId, e);
-        throw new DownloadFileException("Failed to download file from Telegram after retries", e);
     }
 }
