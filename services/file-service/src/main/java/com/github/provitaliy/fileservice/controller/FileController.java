09b79ad064508204c96138816702aac7cbdbe39f
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-12-04 23:53:37 +0500

    file-service: refactor FileController to use FileMetadata and StreamingResponseBody for file downloads

--- services/file-service/src/main/java/com/github/provitaliy/fileservice/controller/FileController.java
+++ services/file-service/src/main/java/com/github/provitaliy/fileservice/controller/FileController.java
@@ -1,7 +1,8 @@
 package com.github.provitaliy.fileservice.controller;
 
-import com.github.provitaliy.fileservice.dto.FileDownloadDTO;
-import com.github.provitaliy.fileservice.service.file.FileAccessService;
+import com.github.provitaliy.fileservice.entity.FileMetadata;
+import com.github.provitaliy.fileservice.service.file.FileMetadataService;
+import com.github.provitaliy.fileservice.service.file.MinioStorageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -10,19 +11,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 @RequiredArgsConstructor
 @RequestMapping("${file-service.download-endpoint}")
 @RestController
 public class FileController {
-    private final FileAccessService accessService;
+    private final MinioStorageService storageService;
+    private final FileMetadataService metadataService;
 
     @GetMapping("{uuid}")
-    public ResponseEntity<byte[]> downloadFile(@PathVariable String uuid) {
-        FileDownloadDTO downloadDto = accessService.getFileByUuid(uuid);
+    public ResponseEntity<StreamingResponseBody> downloadFile(@PathVariable String uuid) {
+        FileMetadata metadata = metadataService.findMetadataByUuid(uuid);
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + downloadDto.fileName() + "\"")
-                .contentType(MediaType.parseMediaType(downloadDto.mimeType()))
-                .body(downloadDto.fileBytes());
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + metadata.getFileName() + "\"")
+                .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(metadata.getFileSize()))
+                .contentType(MediaType.parseMediaType(metadata.getMimeType()))
+                .body(outputStream -> {
+                    try (var is = storageService.getFileStream(metadata.getObjectName())) {
+                        is.transferTo(outputStream);
+                    }
+                });
     }
 }
