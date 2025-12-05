edc04eb13d03a3ef4734433b2d4af77c6428dce9
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-12-05 23:43:40 +0500

    dispatcher: enhance UpdateControllerTest with mock DTOs and message assertions

--- services/dispatcher/src/test/java/com/github/provitaliy/dispatcher/controller/UpdateControllerTest.java
+++ services/dispatcher/src/test/java/com/github/provitaliy/dispatcher/controller/UpdateControllerTest.java
@@ -1,6 +1,12 @@
 package com.github.provitaliy.dispatcher.controller;
 
+import com.github.provitaliy.common.dto.telegram.TelegramDocumentMessageDto;
+import com.github.provitaliy.common.dto.telegram.TelegramPhotoMessageDto;
+import com.github.provitaliy.common.dto.telegram.TelegramTextMessageDto;
+import com.github.provitaliy.dispatcher.mapper.TelegramMessageMapper;
 import com.github.provitaliy.dispatcher.service.UpdateProducer;
+import com.github.provitaliy.dispatcher.util.TestDtoFactory;
+import com.github.provitaliy.dispatcher.util.TestUpdateFactory;
 import com.github.provitaliy.dispatcher.utils.MessageUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,13 +16,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Document;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.message.Message;
-import org.telegram.telegrambots.meta.api.objects.photo.PhotoSize;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
@@ -25,8 +26,6 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UpdateControllerTest {
-    @Mock
-    private MessageUtils messageUtils;
 
     @Mock
     private TelegramClient telegramClient;
@@ -34,92 +33,84 @@ class UpdateControllerTest {
     @Mock
     private UpdateProducer updateProducer;
 
+    @Mock
+    private TelegramMessageMapper messageMapper;
+
     @InjectMocks
     private UpdateController updateController;
 
     @Captor
-    private ArgumentCaptor<Update> updateCaptor;
+    private ArgumentCaptor<TelegramTextMessageDto> textMessageDtoCaptor;
+
+    @Captor
+    private ArgumentCaptor<TelegramDocumentMessageDto> docMessageDtoCaptor;
+
+    @Captor
+    private ArgumentCaptor<TelegramPhotoMessageDto> photoMessageDtoCaptor;
 
     @Captor
     private ArgumentCaptor<SendMessage> sendMessageCaptor;
 
     @Test
-    void shouldProduceTextMessageToQueue() throws Exception {
-        Update textUpdate = new Update();
-        Message textMessage = new Message();
-        textMessage.setText("Hello");
-        textUpdate.setMessage(textMessage);
-        String textQueue = "text.queue";
+    void shouldProduceTextMessageToQueue() {
+        var update = TestUpdateFactory.textUpdate("test message");
+        var expectedDto = TestDtoFactory.textDtoFrom(update);
 
-        updateController.processUpdate(textUpdate);
+        when(messageMapper.toTextMessageDto(update.getMessage()))
+                .thenReturn(expectedDto);
 
-        verify(updateProducer).produceTextMessageUpdate(updateCaptor.capture());
+        updateController.processUpdate(update);
+        verify(updateProducer).produceTextMessageUpdate(textMessageDtoCaptor.capture());
+        TelegramTextMessageDto capturedDto = textMessageDtoCaptor.getValue();
 
-        Update updateToProduce = updateCaptor.getValue();
-
-        assertEquals(textUpdate, updateToProduce);
+        assertEquals(expectedDto, capturedDto);
     }
 
     @Test
     void shouldProduceDocMessageToQueue() {
-        Update docUpdate = new Update();
-        Message docMessage = new Message();
-        docMessage.setDocument(new Document());
-        docUpdate.setMessage(docMessage);
+        Update docUpdate = TestUpdateFactory.docUpdate();
+        var expectedDto = TestDtoFactory.docDtoFrom(docUpdate);
 
-        String docQueue = "doc.queue";
+        when(messageMapper.toDocumentMessageDto(docUpdate.getMessage()))
+                .thenReturn(expectedDto);
 
         updateController.processUpdate(docUpdate);
+        verify(updateProducer).produceDocMessageUpdate(docMessageDtoCaptor.capture());
+        TelegramDocumentMessageDto docMessageToProduce = docMessageDtoCaptor.getValue();
 
-        verify(updateProducer).produceDocMessageUpdate(updateCaptor.capture());
-
-        Update updateToProduce = updateCaptor.getValue();
-
-        assertEquals(docUpdate, updateToProduce);
+        assertEquals(expectedDto, docMessageToProduce);
     }
 
     @Test
     void shouldProducePhotoMessageToQueue() {
-        Update photoUpdate = new Update();
-        Message photoMessage = new Message();
-        List<PhotoSize> photos = List.of(new PhotoSize());
-        photoMessage.setPhoto(photos);
-        photoUpdate.setMessage(photoMessage);
+        Update photoUpdate = TestUpdateFactory.photoUpdate();
+        var expectedDto = TestDtoFactory.photoDtoFrom(photoUpdate);
 
-        String photoQueue = "photo.queue";
+        when(messageMapper.toPhotoMessageDto(photoUpdate.getMessage()))
+                .thenReturn(expectedDto);
 
         updateController.processUpdate(photoUpdate);
+        verify(updateProducer).producePhotoMessageUpdate(photoMessageDtoCaptor.capture());
+        TelegramPhotoMessageDto photoMessageToProduce = photoMessageDtoCaptor.getValue();
 
-        verify(updateProducer).producePhotoMessageUpdate(updateCaptor.capture());
-        Update updateToProduce = updateCaptor.getValue();
-
-        assertEquals(photoUpdate, updateToProduce);
+        assertEquals(expectedDto, photoMessageToProduce);
     }
 
     @Test
     void shouldSendUnsupportedMessageWarningViaTelegram() throws Exception {
-        Update unsupportedUpdate = new Update();
-        Message message = new Message();
-        unsupportedUpdate.setMessage(message);
-
-        String response = "Неподдерживаемый тип сообщения";
-        SendMessage sendMessage = SendMessage.builder()
-                .text(response)
-                .chatId(123L)
-                .build();
-
-        when(messageUtils.generateSendMessageWithText(unsupportedUpdate, response))
-                .thenReturn(sendMessage);
+        Update unsupportedUpdate = TestUpdateFactory.textUpdate(null);
+        String expectedResponse = MessageUtils.UNSUPPORTED_MESSAGE_TYPE_RESPONSE;
 
         updateController.processUpdate(unsupportedUpdate);
         verify(telegramClient).execute(sendMessageCaptor.capture());
         SendMessage messageToSend = sendMessageCaptor.getValue();
-        assertEquals(response, messageToSend.getText());
+
+        assertEquals(expectedResponse, messageToSend.getText());
     }
 
     @Test
     void shouldIgnoreNullUpdate() {
         updateController.processUpdate(null);
-        verifyNoInteractions(updateProducer, messageUtils, telegramClient);
+        verifyNoInteractions(updateProducer, telegramClient);
     }
 }
