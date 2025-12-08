a00c87ee30f0be916e341960cd27be57cd25112d
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-12-08 21:59:34 +0500

    node-service: enhance TestUtils with methods to generate Telegram message DTOs

--- services/node/src/test/java/com/github/provitaliy/node/util/TestUtils.java
+++ services/node/src/test/java/com/github/provitaliy/node/util/TestUtils.java
@@ -1,6 +1,9 @@
 package com.github.provitaliy.node.util;
 
-import com.github.provitaliy.common.event.UserActivatedEvent;
+import com.github.provitaliy.common.dto.telegram.TelegramDocumentMessageDto;
+import com.github.provitaliy.common.dto.telegram.TelegramPhotoMessageDto;
+import com.github.provitaliy.common.dto.telegram.TelegramTextMessageDto;
+import com.github.provitaliy.common.dto.telegram.TelegramUserDto;
 import com.github.provitaliy.node.user.NodeUser;
 import com.github.provitaliy.node.user.UserState;
 import lombok.experimental.UtilityClass;
@@ -21,5 +24,42 @@ public class TestUtils {
                 .isActive(true)
                 .state(UserState.BASIC_STATE)
                 .build();
+    }
+
+    public TelegramTextMessageDto generateTextMessageDto(NodeUser user, String text) {
+        return new TelegramTextMessageDto(
+                user.getChatId(),
+                generateTelegramUserDto(user),
+                text
+        );
+    }
+
+    public TelegramDocumentMessageDto generateDocMessageDto(NodeUser user) {
+        return new TelegramDocumentMessageDto(
+                user.getChatId(),
+                generateTelegramUserDto(user),
+                String.valueOf(faker.number().randomNumber()),
+                faker.file().fileName(),
+                faker.file().mimeType(),
+                faker.number().randomNumber()
+        );
+    }
+
+    public TelegramPhotoMessageDto generatePhotoMessageDto(NodeUser user) {
+        return new TelegramPhotoMessageDto(
+                user.getChatId(),
+                generateTelegramUserDto(user),
+                String.valueOf(faker.number().randomNumber()),
+                faker.number().randomNumber()
+        );
+    }
+
+    public static TelegramUserDto generateTelegramUserDto(NodeUser user) {
+        return new TelegramUserDto(
+                user.getTelegramUserId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getUsername()
+        );
     }
 }
