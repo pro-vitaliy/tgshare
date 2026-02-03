6fbfd591c561609e65ff0e7c777fca3c77632217
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2026-02-04 00:41:15 +0500

    node: add profile annotation to exclude gRPC client configuration in test environment

--- services/node/src/main/java/com/github/provitaliy/node/configuration/GrpcClientConfig.java
+++ services/node/src/main/java/com/github/provitaliy/node/configuration/GrpcClientConfig.java
@@ -6,7 +6,9 @@ import io.grpc.ManagedChannelBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!test")
 @Configuration
 public class GrpcClientConfig {
 
