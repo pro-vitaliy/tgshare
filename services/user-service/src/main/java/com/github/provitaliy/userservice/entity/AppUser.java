32862c18a5fc3f13a36b01081e3c5f9a87f4911b
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2026-02-04 19:58:17 +0500

    user-service: update AppUser entity and table name, add new fields

--- services/user-service/src/main/java/com/github/provitaliy/userservice/entity/AppUser.java
+++ services/user-service/src/main/java/com/github/provitaliy/userservice/entity/AppUser.java
@@ -1,5 +1,6 @@
 package com.github.provitaliy.userservice.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,7 +20,7 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "app_user")
+@Table(name = "app_users")
 @Entity
 public class AppUser {
 
@@ -27,15 +28,35 @@ public class AppUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(
+            name = "telegram_user_id",
+            unique = true,
+            nullable = false,
+            updatable = false
+    )
     private Long telegramUserId;
+
+    @Column(name = "chat_id")
     private Long chatId;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
     private String username;
+
+    @Column(unique = true)
     private String email;
+
+    @Column(name = "unconfirmed_email")
     private String unconfirmedEmail;
+
+    @Column(name = "is_active")
     private Boolean isActive;
 
     @CreationTimestamp
+    @Column(name = "first_login_date", nullable = false, updatable = false)
     private LocalDateTime firstLoginDate;
 }
